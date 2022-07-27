e7ca96cfe5935a060d0c127e92ee9736afc88f28
Thomas Aleknovic <aleknovic70@gmail.com>
2022-07-27 15:44:44 -0300

    Header Update

--- src/components/Header/HeaderComponent.tsx
+++ src/components/Header/HeaderComponent.tsx
@@ -3,9 +3,9 @@ import InputComponent from './InputComponent'
 
 const HeaderComponent: React.FC<any> = ({handleForecast}) => {
   return (
-    <header className='bg-main-blue p-7 grid grid-flow-col grid-cols-2 justify-around'>
-        <h1 className="font-extrabold">The Weather App</h1>
-        <div className='flex justify-center'>
+    <header className='bg-main-blue p-7 grid grid-flow-col grid-cols-2 justify-around items-center'>
+        <h1 className="font-extrabold text-xl">The Weather App</h1>
+        <div className='flex justify-end mr-5'>
         <InputComponent handleForecast={handleForecast}/>  
         </div>
 
